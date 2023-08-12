b8f879530b48943bbf33d534fffc83a474e1973c
mmohgaomar25 <mohgaomar25@gmail.com>
2023-08-11 17:12:41 -0700

    fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point of the program
  *
@@ -9,19 +8,15 @@
  */
 int main(void)
 {
-    int i;
-
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');
-    }
-
-    for (i = 'a'; i <= 'f'; i++)
-    {
-        putchar(i);
-    }
-
-    putchar('\n');
-
-    return (0);
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+for (i = 'a'; i <= 'f'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
