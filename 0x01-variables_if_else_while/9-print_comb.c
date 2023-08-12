7ca7a46691352c1454222e33fe67bf8868801c31
mmohgaomar25 <mohgaomar25@gmail.com>
2023-08-11 17:52:08 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,12 +14,11 @@ int main(void)
         putchar((num % 10) + '0');
         if (num == 9)
             continue;
-
         putchar(',');
         putchar(' ');
     }
-
+    
     putchar('\n');
-
+    
     return (0);
 }
