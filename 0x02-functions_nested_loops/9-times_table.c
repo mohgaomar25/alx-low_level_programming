8d2a023f64aa5d165c2dedca3d1168a4201aa2eb
mmohgaomar25 <mohgaomar25@gmail.com>
2023-08-15 17:52:10 -0700

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,35 +5,40 @@
  */
 void times_table(void)
 {
-	int row, column, product;
+    int row, column, product;
 
-	for (row = 0; row <= 9; row++)
-	{
-		for (column = 0; column <= 9; column++)
-		{
-			product = row * column;
-			if (column == 0)
-			{
-				_putchar('0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+    for (row = 0; row <= 9; row++)
+    {
+        for (column = 0; column <= 9; column++)
+        {
+            product = row * column;
+            if (column == 0)
+            {
+                _putchar('0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
 
-			if (product < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (product >= 10)
-			{
-				_putchar(' ');
-			}
+                if (product < 10)
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                else
+                {
+                    _putchar(' ');
+                }
+            }
 
-			_putchar((product % 10) + '0');
-		}
-		_putchar('\n');
-	}
+            _putchar((product % 10) + '0');
+
+            if (column < 9)
+            {
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
 }
