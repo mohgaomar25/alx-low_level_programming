5b695b0dffd7dbe15f6759673c0b57ba5595d5c9
mmohgaomar25 <mohgaomar25@gmail.com>
2023-08-15 18:15:01 -0700

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,32 @@
  */
 void times_table(void)
 {
-    int row, col, product;
+	int row, col, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (col = 0; col <= 9; col++)
-        {
-            product = row * col;
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			product = row * col;
 
-            if (col == 0)
-            {
-                _putchar(product + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (col == 0)
+			{
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                if (product < 10)
-                    _putchar(' ');
+				if (product < 10)
+					_putchar(' ');
 
-                if (product >= 10)
-                    _putchar(product / 10 + '0');
+				if (product >= 10)
+					_putchar(product / 10 + '0');
 
-                _putchar(product % 10 + '0');
-            }
-        }
-        _putchar('\n');
-    }
+				_putchar(product % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
