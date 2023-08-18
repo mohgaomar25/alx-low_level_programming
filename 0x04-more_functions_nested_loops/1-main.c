c58f5b9629fa16eaf0da516134e8c6798812f506
mmohgaomar25 <mohgaomar25@gmail.com>
2023-08-17 18:29:43 -0700

    Write a function that checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char c;
+	
+	c = '0';
+	_putchar(c);
+	_putchar(':');
+	_putchar(' ');
+	_putchar(_isdigit(c) + '0');
+	_putchar('\n');
+
+	c = 'a';
+	_putchar(c);
+	_putchar(':');
+	_putchar(' ');
+	_putchar(_isdigit(c) + '0');
+	_putchar('\n');
+
+	return (0);
+}
