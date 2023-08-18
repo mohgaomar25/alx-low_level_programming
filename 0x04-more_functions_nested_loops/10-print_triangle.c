b692ef50053e5e40e0ee85b42fec52a6583e25a0
mmohgaomar25 <mohgaomar25@gmail.com>
2023-08-17 19:27:33 -0700

    fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,24 +7,27 @@
 
 void print_triangle(int size)
 {
+	int i;
+	int j;
+	int k;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
-	}
-	
-	int i, j;
-
-	for (i = 0; i < size; i++)
+	} else
 	{
-		for (j = 0; j < size - i - 1; j++)
+
+		for (i = 1; i <= size; i++)
 		{
-			_putchar(' ');
+			for (j = size - i ; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		for (j = 0; j < i + 1; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
 	}
 }
