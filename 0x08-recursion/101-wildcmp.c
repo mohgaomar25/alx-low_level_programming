f29373b6d5cf1dd261e4a85406578271fb724402
mmohgaomar25 <mohgaomar25@gmail.com>
2023-08-30 18:00:49 -0700

    fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,22 +5,24 @@
  * @s1: The first string.
  * @s2: The second string with wildcards.
  * Return: 1 if the strings can be considered identical, 0 otherwise.
- */
+*/
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
 	if (*s2 == '*')
 	{
-		if (!*s1 && !*(s2 + 1))
-			return (1);
-		if (*s1 && !wildcmp(s1 + 1, s2))
-			return (wildcmp(s1, s2 + 1));
-		if (!*s1 && wildcmp(s1, s2 + 1))
-			return (1);
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
-	if (*s1 == *s2)
+	else if (*s1 == *s2)
 	{
-		if (!*s1)
-			return (1);
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
