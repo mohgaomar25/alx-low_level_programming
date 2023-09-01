01ee87e2e1d8b468e07b865edfcfdfb239e08690
mmohgaomar25 <mohgaomar25@gmail.com>
2023-09-01 15:30:31 -0700

    program that prints the minimum number of coins to make change for an amount of money

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_positive_number - Check if a string represents a positive number.
+ * @s: The string to check
+ *
+ * Return: 1 if it's a positive number, 0 otherwise
+ */
+int is_positive_number(char *s)
+{
+	while (*s)
+	{
+		if (*s < '0' || *s > '9')
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (is_positive_number(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
