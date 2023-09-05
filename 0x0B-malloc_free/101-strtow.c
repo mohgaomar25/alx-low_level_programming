a3ef6763ce4d9643eae047b5b88d3f87c6a88765
mmohgaomar25 <mohgaomar25@gmail.com>
2023-09-05 15:31:16 -0700

    Write a function that splits a string into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,83 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * count_words - Counts the number of words in a string
+ * @str: The input string
+ *
+ * Return: The number of words in the string
+ */
+int count_words(char *str)
+{
+	int count = 0, in_word = 0;
+
+	while (*str)
+	{
+		if (*str == ' ' || *str == '\t' || *str == '\n')
+		{
+			if (in_word)
+			{
+				in_word = 0;
+			}
+		}
+		else if (!in_word)
+		{
+			in_word = 1;
+			count++;
+		}
+		str++;
+	}
+
+	return (count);
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The input string
+ *
+ * Return: A pointer to an array of strings (words), or NULL if it fails
+ */
+char **strtow(char *str)
+{
+	int i, j, len = 0, word_count = 0;
+	char **words;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	word_count = count_words(str);
+
+	words = malloc(sizeof(char *) * (word_count + 1));
+
+	if (words == NULL)
+		return (NULL);
+
+	for (i = 0; i < word_count; i++)
+	{
+		while (*str == ' ' || *str == '\t' || *str == '\n')
+			str++;
+
+		for (len = 0; str[len] && str[len] != ' ' && str[len] != '\t' && str[len] != '\n'; len++)
+			;
+
+		words[i] = malloc(sizeof(char) * (len + 1));
+
+		if (words[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(words[j]);
+			free(words);
+			return (NULL);
+		}
+
+		for (j = 0; j < len; j++, str++)
+			words[i][j] = *str;
+
+		words[i][j] = '\0';
+	}
+
+	words[word_count] = NULL;
+
+	return (words);
+}
