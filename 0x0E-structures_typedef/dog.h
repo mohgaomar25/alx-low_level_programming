354f4dc05837c4208befaaf8b135d236b589db33
mmohgaomar25 <mohgaomar25@gmail.com>
2023-09-11 11:46:48 -0700

    Define a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a structure for a dog
+ * @name: The name of the dog (string)
+ * @age: The age of the dog (float)
+ * @owner: The owner of the dog (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
