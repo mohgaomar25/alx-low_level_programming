d807d03bc08eae1e23f39b903c17d11674d49514
mmohgaomar25 <mohgaomar25@gmail.com>
2023-09-21 16:53:21 -0700

    last adv. task

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+    hello db "Hello, Holberton",10,0 ; The string to be printed
+    format db "%s",0               ; Format string for printf
+
+section .text
+    extern printf
+
+global main
+
+main:
+    ; Call printf function to print the string
+    push hello
+    push format
+    call printf
+
+    ; Exit the program
+    mov rax, 60         ; syscall: exit
+    xor rdi, rdi        ; status: 0
+    syscall
