9d3bfd41020c5be9ffcb226a792ec1a4d5275b62
mmohgaomar25 <mohgaomar25@gmail.com>
2023-09-21 16:56:04 -0700

    fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,19 @@
 section .data
-    hello db "Hello, Holberton",10,0 ; The string to be printed
-    format db "%s",0               ; Format string for printf
+hello db "Hello, Holberton",10,0 ; The string to be printed
+format db "%s",0               ; Format string for printf
 
 section .text
-    extern printf
+extern printf
 
 global main
 
 main:
-    ; Call printf function to print the string
-    push hello
-    push format
-    call printf
+; Call printf function to print the string
+push hello
+push format
+call printf
 
-    ; Exit the program
-    mov rax, 60         ; syscall: exit
-    xor rdi, rdi        ; status: 0
-    syscall
+; Exit the program
+mov rax, 60         ; syscall: exit
+xor rdi, rdi        ; status: 0
+syscall
