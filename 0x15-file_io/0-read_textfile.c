1626007e70bd50ba4307e6f727c3332b747a32b9
mohgaomar25 <138797863+mohgaomar25@users.noreply.github.com>
2023-10-03 00:02:03 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,30 +10,30 @@
  * @letters: The number of letters to read and print.
  *
  * Return: The actual number of letters it could read and print.
- *         If the file cannot be opened or read, return 0.
- *         If filename is NULL, return 0.
- *         If write fails or does not write the expected amount of bytes, return 0.
+ * If the file cannot be opened or read, return 0.
+ * If filename is NULL, return 0.
+ * If write fails or does not write the expected amount of bytes, return 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     int file_descriptor;
     ssize_t bytes_read, bytes_written;
     char *buffer;
-
+    
     if (filename == NULL)
         return (0);
-
+    
     file_descriptor = open(filename, O_RDONLY);
     if (file_descriptor == -1)
         return (0);
-
+    
     buffer = malloc(sizeof(char) * letters);
     if (buffer == NULL)
     {
         close(file_descriptor);
         return (0);
     }
-
+    
     bytes_read = read(file_descriptor, buffer, letters);
     if (bytes_read == -1)
     {
@@ -41,7 +41,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
         free(buffer);
         return (0);
     }
-
+    
     bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
     if (bytes_written == -1 || bytes_written != bytes_read)
     {
@@ -49,9 +49,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
         free(buffer);
         return (0);
     }
-
+    
     close(file_descriptor);
     free(buffer);
-
+    
     return (bytes_written);
 }
