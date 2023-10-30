6fc65ddeeb54c37df85d19e688aef927487039ea
mohgaomar25 <mohgaomar25@gmail.com>
2023-10-30 13:55:06 -0700

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,53 +5,7 @@
 #include "main.h"
 
 /**
- * copyFile - Copies the content from one file to another.
- * @source: The source file path.
- * @destination: The destination file path.
- *
- * Return: 0 on success, or the appropriate error code on failure.
- */
-int copyFile(const char *source, const char *destination)
-{
-	int fd_from, fd_to;
-	char buffer[1024];
-	ssize_t rd_bytes, wr_byte;
-
-	fd_from = open(source, O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", source);
-		return (98);
-	}
-
-	fd_to = open(destination, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", destination);
-		close(fd_from);
-		return (99);
-	}
-
-	while (0rd_bytes = read(fd_from, buffer, 1024) > 0)
-	{
-		wr_bytes = write(fd_to, buffer, rd_bytes);
-		if (wr_bytes == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", destination);
-			close(fd_from);
-			close(fd_to);
-			return (99);
-		}
-	}
-
-	close(fd_from);
-	close(fd_to);
-
-	return (0);
-}
-
-/**
- * main - Entry point of the program.
+ * main - Copies the content of a file to another file.
  * @ac: The number of arguments.
  * @av: The array of arguments.
  *
@@ -59,13 +13,57 @@ int copyFile(const char *source, const char *destination)
  */
 int main(int ac, char *av[])
 {
+	int fd_from, fd_to, rd_bytes, wr_bytes;
+	char buffer[1024];
+
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
-		return (97);
+		exit(97);
 	}
 
-	int result = copyFile(av[1], av[2]);
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
-	return (result);
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	do
+	{
+		rd_bytes = read(fd_from, buffer, 1024);
+		if (rd_bytes == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+
+		wr_bytes = write(fd_to, buffer, rd_bytes);
+		if (wr_bytes == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	} while (rd_bytes > 0);
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
 }
